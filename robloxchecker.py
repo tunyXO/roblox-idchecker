55fb1a5a14ae867d8c506c78ff9daf9bf1e3dbb9
tuny <126991086+tuny1337@users.noreply.github.com>
2023-03-25 16:07:06 +0100

    Add files via upload

--- robloxchecker.py
+++ robloxchecker.py
@@ -1,11 +1,12 @@
 # Simple ROBLOX user info retriever via id.
+# Written by tuny
 
 import requests
 
 thumbnail_url = 'https://thumbnails.roblox.com/v1/users/avatar?userIds='
 url = 'https://users.roblox.com/v1/users/'
 
-id = '1' # Input your desired ID here
+id = input('Enter ID: ')
 size = '420x420' # Sizes: 720x720, 420x420, 352x552...
 format = 'Png' # Png or Jpeg
 is_circular = 'false' # Determines if the thumbnail is Circular or not
@@ -16,9 +17,9 @@ if userinfo.status_code == 200:
   username = data['name']
   display_name = data['displayName']
   description = data['description']
-  print(f"Username: {username}")
-  print(f"Display name: {display_name}")
-  print(f"description: {description}")
+  print(f'Username: {username}')
+  print(f'Display name: {display_name}')
+  print(f'description: {description}')
 else:
   print('Error retrieving user info')
 
@@ -26,17 +27,17 @@ userhistory = requests.get(url+id+'/username-history')
 if userhistory.status_code == 200:
   data = userhistory.json()
 else:
-  print("Error retrieving username history")
+  print('Error retrieving username history')
 print('Past usernames:', end= ' ')
-for username_data in data["data"]:
-    username = username_data["name"]
+for username_data in data['data']:
+    username = username_data['name']
     print(username, end= ' ')
-    
+
 userthumbnail = requests.get(thumbnail_url+id+'&size='+size+'&format='+format+'&isCircular='+is_circular)
 if userthumbnail.status_code == 200:
   data = userthumbnail.json()
-  image = data["data"][0]["imageUrl"]
-  print(image)
+  image = data['data'][0]['imageUrl']
+  print('\n'+image)
 else:
   print('Error retrieving user thumbnail')
 
